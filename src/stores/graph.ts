5fec0bd59ad7337e7a697498444e7cee5b9f3a0c
Christian Glassiognon <christian@heyglassy.co>
2022-05-21 15:51:32 -0400

    Renamed store2 to store

--- src/stores/graph.ts
+++ src/stores/graph.ts
@@ -1,19 +1,20 @@
 import { graphs } from "../types/graphs";
 import { GetState, SetState } from "zustand";
-import { MyState } from "../types/store2";
+import { MyState } from "../types/store";
 
-const Graphs = ((set: SetState<MyState>, get: GetState<MyState>): graphs => ({
-    graphData: [],
-    plotPoint: { x: 1, y: 1 },
-    set: (update) => set({ graphs: { ...get().graphs, ...update } }),
-    update: (plotPoint) => set(({ graphs: graphs }) => {
-        graphs.plotPoint = plotPoint;
-        graphs.current?.data.datasets[0].data.push(plotPoint);
-        graphs.current?.data.datasets[1].data.pop();
-        graphs.current?.data.datasets[1].data.push(plotPoint);
-        graphs.current?.update("none");
-        graphs.graphData.concat(plotPoint);
+const Graphs = (set: SetState<MyState>, get: GetState<MyState>): graphs => ({
+  graphData: [],
+  plotPoint: { x: 1, y: 1 },
+  set: (update) => set({ graphs: { ...get().graphs, ...update } }),
+  update: (plotPoint) =>
+    set(({ graphs: graphs }) => {
+      graphs.plotPoint = plotPoint;
+      graphs.current?.data.datasets[0].data.push(plotPoint);
+      graphs.current?.data.datasets[1].data.pop();
+      graphs.current?.data.datasets[1].data.push(plotPoint);
+      graphs.current?.update("none");
+      graphs.graphData.concat(plotPoint);
     }),
-}));
+});
 
-export default Graphs
+export default Graphs;
