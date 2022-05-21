c08ca8aed989b2488fcaa5060b42130fa4c68996
Christian Glassiognon <christian@heyglassy.co>
2022-05-21 15:59:37 -0400

    Formatted all type files

--- src/types/graphs.ts
+++ src/types/graphs.ts
@@ -4,9 +4,9 @@ import { Optional } from "./utils";
 export type plotPoint = { x: number; y: number }; // x = temperature y = magnetization
 
 export interface graphs {
-    current?: Chart<"scatter", Array<plotPoint>, unknown>;
-    plotPoint: plotPoint
-    graphData: Array<plotPoint>;
-    set: (update: Optional<graphs>) => void;
-    update: (plotPoint: plotPoint) => void;
+  current?: Chart<"scatter", Array<plotPoint>, unknown>;
+  plotPoint: plotPoint;
+  graphData: Array<plotPoint>;
+  set: (update: Optional<graphs>) => void;
+  update: (plotPoint: plotPoint) => void;
 }
--- src/types/simulation.ts
+++ src/types/simulation.ts
@@ -1,25 +1,27 @@
 import { Optional } from "./utils";
 
+// TODO: Union type on currentURL
+
 export interface simulation {
-    currentUrl: string;
-    localMagnetic: Array<Array<number>>;
-    spin: Array<any>;
-    spins: Array<Array<number>>;
-    spinBefore: Array<any>;
-    nearestNeighs: Object;
-    clusteredChildren: Array<any>;
-    wall: Array<Array<number>>;
-    running: boolean,
-    freePlay: boolean,
-    freePlayIncrememt: boolean,
-    frames: Array<string>;
-    temperature: number;
-    calcStats: any
-    algo: any
-    energy: number,
-    totalEnergy: number,
-    magnetism: number,
-    totalMagnetism: number,
-    loopCount: number,
-    set: (update: Optional<simulation>) => void;
+  currentUrl: string;
+  localMagnetic: Array<Array<number>>;
+  spin: Array<any>;
+  spins: Array<Array<number>>;
+  spinBefore: Array<any>;
+  nearestNeighs: Object;
+  clusteredChildren: Array<any>;
+  wall: Array<Array<number>>;
+  running: boolean;
+  freePlay: boolean;
+  freePlayIncrememt: boolean;
+  frames: Array<string>;
+  temperature: number;
+  calcStats: any;
+  algo: any;
+  energy: number;
+  totalEnergy: number;
+  magnetism: number;
+  totalMagnetism: number;
+  loopCount: number;
+  set: (update: Optional<simulation>) => void;
 }
