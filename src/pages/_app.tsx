0bd19cc65d8e68a982e5275478bd342fb7142f16
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-01-06 23:24:01 -0800

    get rid of TRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { withTRPC } from "@trpc/next";
-import { AppRouter } from "./api/trpc/[trpc]";
 import Layout from "../components/Layouts";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
@@ -13,19 +12,4 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-export default withTRPC<AppRouter>({
-  config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
-    return {
-      url,
-      links: [
-        httpLink({
-          url: "/api/trpc",
-        }),
-      ],
-    };
-  },
-  ssr: false,
-})(MyApp);
+export default MyApp;
